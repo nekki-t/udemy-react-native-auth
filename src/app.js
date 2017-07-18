cb53c594c2000211703956ed4005febe9ebe4ae5
nekki-t <nekki.t.0317@gmail.com>
2017-07-19 07:30:26 +0900

    init

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication"/>
+        <LoginForm/>
+      </View>
+    )
+  }
+}
+
+export default App;
